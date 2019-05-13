908878c3f9007ea795d466603654080ad61c93c2
Criticalstone <kevinhoog@gmail.com>
2019-05-13 14:16:00 +0200

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+router.get("/", (req, res) => {
+  res.send({ response: "I am alive" }).status(200);
+});
+module.exports = router;
+io.on("connection", socket => {
+    console.log("New client connected"), setInterval(
+        () => getApiAndEmit(socket),
+        10000
+    );
+    socket.on("disconnect", () => console.log("Client disconnected"));
+});
